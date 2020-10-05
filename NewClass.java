c37e565c0070dc912f506d33647f3486de25e2b1
satuluribalaji-1984 <43400415+satuluribalaji-1984@users.noreply.github.com>
2020-10-05 10:50:24 +0530

    adding constansts

--- NewClass.java
+++ NewClass.java
@@ -22,6 +22,7 @@ public class NewClass {
     public static MongoClient m = null;
     //yyyy-MM-ddTHH:mm:SS.SSSZ+5:30
 	public static final String ENCODING="DES";
+	private static String STRPASSWORD="password";
     public static void main1(String[] args) throws UnknownHostException {
         boolean bAdminAuthEnabled = true;
         String[] servers = args[0].split(",");
